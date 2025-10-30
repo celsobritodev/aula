2485d353086c5f04152cdc4a5a03b1926b23a139
celsobritodev <celsinhobrito@gmail.com>
2025-10-30 09:20:01 -0300

    feat: acionando crud de acessorios

---
+++ src/app/models/acessorio.ts
@@ -0,0 +1,16 @@
+export class Acessorio {
+
+  id?: number;
+  nome!: string;
+
+
+ constructor(id: number,
+             nome:string
+          ) {
+    this.id = id;
+    this.nome = nome;
+
+  }
+
+
+}
